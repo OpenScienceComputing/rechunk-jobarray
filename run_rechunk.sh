fdd29ab199b036028fb84cdaa8b8316e78986721
Rich Signell <rsignell@gmail.com>
2024-12-17 15:08:36 -0500

    add spot

--- run_rechunk.sh
+++ run_rechunk.sh
@@ -2,7 +2,8 @@
 #COILED ntasks 19  
 #COILED workspace esip-lab
 #COILED software pangeo-arm
-#COILED region us-east-1
+#COILED region us-west-2
 #COILED arm True
 #COILED vm-type r7g.8xlarge
+#COILED spot_policy spot_with_fallback
 python ERA5-rechunker-AWS.py
