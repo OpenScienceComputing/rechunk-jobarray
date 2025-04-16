004e301774f0bb74996895417e1a99ba404c6512
Rich Signell <rsignell@gmail.com>
2025-04-16 11:53:25 -0400

    removed old scripts

--- run_rechunk.sh
+++
@@ -1,8 +0,0 @@
-#COILED disk-size 120GB
-#COILED ntasks 19  
-#COILED workspace esip-lab
-#COILED software pangeo-arm
-#COILED region us-west-2
-#COILED arm True
-#COILED vm-type r7g.8xlarge
-python ERA5-rechunker-AWS-s3.py
